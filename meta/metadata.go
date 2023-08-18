7e7bea5d529d6f8a292de81d7f7a0d584c4abc43
huantedness <keikoamo@163.com>
2023-08-18 22:16:40 +0800

    add optional caller args

--- meta/metadata.go
+++ meta/metadata.go
@@ -6,26 +6,45 @@ import (
 	"strings"
 )
 
+type CallerOption int
+
+const (
+	// apply function name to result
+	WithoutFunctionName CallerOption = 1 << iota
+	// apply file path to result
+	WithFilePath
+)
+
 // Caller return file and line number information
 // based on [runtime.Caller]
 // but in string form with at most 3-tier directories
-func Caller() string {
-	_, file, line, _ := runtime.Caller(1)
-	buf := &strings.Builder{}
-	writeTrimmedPath(buf, file)
-	buf.WriteByte(':')
-	buf.WriteString(strconv.Itoa(line))
-	return buf.String()
+func Caller(options ...CallerOption) string {
+	return CallerSkip(1, options...)
 }
 
 // CallerSkip is similar to Caller but with skip specified
 //
 //	note: meta.CallerSkip(1) is equivalent to runtime.Caller(0)
-func CallerSkip(skip int) string {
-	_, file, line, _ := runtime.Caller(skip)
+func CallerSkip(skip int, options ...CallerOption) string {
+	var option CallerOption
+	for _, op := range options {
+		option |= op
+	}
+	pc, file, line, _ := runtime.Caller(skip)
 	buf := &strings.Builder{}
-	writeTrimmedPath(buf, file)
-	buf.WriteByte(':')
+	// apply function name to result
+	if option&WithoutFunctionName != WithoutFunctionName {
+		fn := runtime.FuncForPC(pc)
+		if fn != nil {
+			buf.WriteString(fn.Name())
+			buf.WriteByte(':')
+		}
+	}
+	// apply file path to result
+	if option&WithFilePath == WithFilePath {
+		writeTrimmedPath(buf, file)
+		buf.WriteByte(':')
+	}
 	buf.WriteString(strconv.Itoa(line))
 	return buf.String()
 }
