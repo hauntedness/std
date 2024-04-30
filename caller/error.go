68a8f88fd258cf59cfd77db1abd84b6f2c959cf4
hauntedness <59566708+hauntedness@users.noreply.github.com>
2024-04-30 21:14:13 +0800

    remove exhaustive error wrapper

--- caller/error.go
+++ caller/error.go
@@ -2,8 +2,20 @@ package caller
 
 import (
 	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 func Error(err error) error {
-	return fmt.Errorf("%s, %w", NameSkip(2), err)
+	pc, _, line, _ := runtime.Caller(1)
+	buf := &strings.Builder{}
+	// apply function name to result
+	fn := runtime.FuncForPC(pc)
+	if fn != nil {
+		buf.WriteString(fn.Name())
+		buf.WriteByte(':')
+		buf.WriteString(strconv.Itoa(line))
+	}
+	return fmt.Errorf("%s: %w", buf.String(), err)
 }
