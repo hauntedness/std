b19f6c7d151283533cfa8c73643b36db5e69ddd5
hauntedness <59566708+hauntedness@users.noreply.github.com>
2025-04-13 13:06:27 +0800

    remove required

--- hv/explicit.go
+++ hv/explicit.go
@@ -1,34 +1,20 @@
 package hv
 
-// np prevent other packages from implementing IsOption and IsRequired.
-type np struct{}
+// boolean prevent other packages from implementing IsOption and IsRequired.
+type boolean struct{}
 
 // IsOption is used to distinguish Option[T] with other types.
 // you may need this in rare corner cases.
 // e.g. when you want to test a reflect.Value is hv.Option.
 // It is hard to instantiate all possible hv.Option[T].
 type IsOption interface {
-	isOption(np)
+	isOption() boolean
 }
 
 var _ IsOption = Option[struct{}]{}
 
 // Is implements IsOption.
-func (o Option[T]) isOption(np) {
+func (o Option[T]) isOption() boolean {
 	// see interface IsOption.
-}
-
-// IsRequired is used to distinguish Required[T] with other types.
-// you may need this in rare corner cases.
-// e.g. when you want to test a reflect.Value is hv.Required.
-// It is hard to instantiate all possible hv.Required[T].
-type IsRequired interface {
-	isRequired(np)
-}
-
-var _ IsRequired = Required[struct{}]{}
-
-// Is implements IsRequired.
-func (o Required[T]) isRequired(np) {
-	// see interface IsRequired.
+	return boolean{}
 }
