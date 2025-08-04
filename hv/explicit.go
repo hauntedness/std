f74a49429e6b21bbaa895ebd83f646389bb3e3dc
hauntedness <59566708+hauntedness@users.noreply.github.com>
2025-08-04 19:04:29 +0800

    refine tag type

--- hv/explicit.go
+++ hv/explicit.go
@@ -1,20 +1,14 @@
 package hv
 
-// boolean prevent other packages from implementing IsOption and IsRequired.
-type boolean struct{}
-
-// IsOption is used to distinguish Option[T] with other types.
+// HvOption is used to distinguish Option[T] with other types.
 // you may need this in rare corner cases.
 // e.g. when you want to test a reflect.Value is hv.Option.
 // It is hard to instantiate all possible hv.Option[T].
-type IsOption interface {
-	isOption() boolean
+type HvOption interface {
+	isOption()
 }
 
-var _ IsOption = Option[struct{}]{}
+var _ HvOption = Option[struct{}]{}
 
 // Is implements IsOption.
-func (o Option[T]) isOption() boolean {
-	// see interface IsOption.
-	return boolean{}
-}
+func (o Option[T]) isOption() {}
