7a54d75c8f57d6ad9e15508f2dfa07d6195e3d5b
hauntedness <hauntedness@hauntednessdeMac-mini.local>
2025-10-09 20:34:32 +0800

    refactor errors

--- fmts/fmts.go
+++ fmts/fmts.go
@@ -13,16 +13,16 @@ func Error(err error) error {
 	return &StructError{error: err, stack: callers(), msg: "no message"}
 }
 
-// Errorf formats according to a format specifier and returns the string
+// Errors formats according to a format specifier and returns the string
 // as a value that satisfies error.
-// Errorf also records the stack trace at the point it was called.
-func Errorf(format string, args ...any) error {
+// Errors also records the stack trace at the point it was called.
+func Errors(format string, args ...any) error {
 	return &StructError{error: ErrAnonymouse, msg: fmt.Sprintf(format, args...), stack: callers()}
 }
 
-// ErrorWith wraps an error into a StructError, appending a formatted message.
+// Errorf wraps an error into a StructError, appending a formatted message.
 // If the error is already a StructError, it appends the message to the existing one.
-func ErrorWith(err error, format string, args ...any) error {
+func Errorf(err error, format string, args ...any) error {
 	var ws = &StructError{}
 	if errors.As(err, &ws) {
 		ws.msg = ws.msg + ": " + fmt.Sprintf(format, args...)
