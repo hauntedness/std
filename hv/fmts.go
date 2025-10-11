afe4ea261319184d43a698a66d57f228b587ffb0
hauntedness <hauntedness@hauntednessdeMac-mini.local>
2025-10-11 13:14:37 +0800

    error

--- hv/fmts.go
+++ hv/fmts.go
@@ -5,21 +5,21 @@ import (
 	"fmt"
 )
 
-// Err wraps an error into a [Error], recording the stack trace and a default message.
-func Err(err error) error {
+// Errv wraps an error into a [Error], recording the stack trace and a default message.
+func Errv(err error) error {
 	return &Error{error: err, stack: callers(), msg: "..."}
 }
 
-// Errorx formats according to a format specifier and returns the string
+// Errx formats according to a format specifier and returns the string
 // as a value that satisfies error.
-// Errorx also records the stack trace at the point it was called.
-func Errorx(format string, args ...any) error {
+// Errx also records the stack trace at the point it was called.
+func Errx(format string, args ...any) error {
 	return &Error{error: fmt.Errorf(format, args...), stack: callers(), msg: "..."}
 }
 
-// Errorf wraps an error into a [Error], appending a formatted message.
+// Errf wraps an error into a [Error], appending a formatted message.
 // If the error is already a [Error], it appends the message to the existing one.
-func Errorf(err error, format string, args ...any) error {
+func Errf(err error, format string, args ...any) error {
 	var ws = &Error{}
 	if errors.As(err, &ws) {
 		ws.msg = ws.msg + ": " + fmt.Sprintf(format, args...)
