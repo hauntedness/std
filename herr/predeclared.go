cca72dda55c1cae234fc3403e28629f90a7434cc
hauntedness <59566708+hauntedness@users.noreply.github.com>
2025-11-15 22:14:00 +0800

    refactor error

--- herr/predeclared.go
+++ herr/predeclared.go
@@ -15,4 +15,4 @@ var ErrUnexpected = errors.New("unexpected error")
 // ErrNew indicates that the error is Guard Clause Error. You do only care about it's nil or not.
 //
 // Functions and methods should not return this error but should instead return a wrap of this one.
-var ErrNew = errors.New("other error")
+var ErrNew = errors.New("some error")
