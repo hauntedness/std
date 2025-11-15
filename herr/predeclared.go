ab62ffe1dce9024e774d3c6bef69e201d9fb1e80
hauntedness <59566708+hauntedness@users.noreply.github.com>
2025-11-15 22:09:46 +0800

    refactor error

--- herr/predeclared.go
+++ herr/predeclared.go
@@ -12,7 +12,7 @@ var ErrInvalid = errors.New("value is invalid")
 // Functions and methods should not return this error but should instead return a wrap of this one.
 var ErrUnexpected = errors.New("unexpected error")
 
-// ErrNew indicates that the error is Guard Clause Error. You do not need to
+// ErrNew indicates that the error is Guard Clause Error. You do only care about it's nil or not.
 //
 // Functions and methods should not return this error but should instead return a wrap of this one.
 var ErrNew = errors.New("other error")
